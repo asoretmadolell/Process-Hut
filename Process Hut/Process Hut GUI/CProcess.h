37e59ee0cafb46c3cd4816b663e9410c565e4524
asoretmadolell <soret_40@hotmail.com>
2019-12-08 17:22:49 +0100

    Printing a list of processes in a Tree Control. First attempt.

---
+++ Process Hut/Process Hut GUI/CProcess.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <windows.h>
+
+/***************************************************************************/
+/*                                                                         */
+/*                                                                         */
+/*                                                                         */
+/* CProcess Class                                                          */
+/*                                                                         */
+/*                                                                         */
+/*                                                                         */
+/***************************************************************************/
+class CProcess
+{
+private:
+    int         m_ProcessId;
+    DWORD       m_ProcessName;
+
+public:
+    CProcess( int ProcessId );
+    virtual     ~CProcess();
+
+    int         GetProcessId() { return m_ProcessId; }
+    void        SetProcessId( int ProcessId ) { m_ProcessId = ProcessId; }
+    DWORD       GetProcessName() { return m_ProcessName; }
+    void        SetProcessName( DWORD ProcessName ) { m_ProcessName = ProcessName; }
+};
--- Process Hut/Process Hut GUI/CProcessHutGUIDlg.cpp
+++ Process Hut/Process Hut GUI/CProcessHutGUIDlg.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "CProcessHutGUIDlg.h"
 #include "CProcessHutGUIApp.h"
+#include "CProcess.h"
+#include <psapi.h>
+#include <string>
 
 // CProcessHutGUIDlg dialog
 
@@ -23,35 +26,32 @@ void CProcessHutGUIDlg::DoDataExchange( CDataExchange* pDX )
 BOOL CProcessHutGUIDlg::OnInitDialog()
 {
     CDialog::OnInitDialog();
-    
+
+    // Get the list of process identifiers.
+    DWORD aProcesses[ 1024 ], cbNeeded, cProcesses;
+    unsigned int i;
+
+    if( !EnumProcesses( aProcesses, sizeof( aProcesses ), &cbNeeded ) )
+    {
+        return 1;
+    }
+
+    // Calculate how many process identifiers were returned.
+    cProcesses = cbNeeded / sizeof( DWORD );
     ShowWindow( SW_MINIMIZE );
 
-    HTREEITEM hItem, hCar;
-    hItem = m_treeCtrl.InsertItem( L"Car Listing", TVI_ROOT );
-    hCar = m_treeCtrl.InsertItem( L"Economy", hItem );
-    m_treeCtrl.InsertItem( L"BH-733", hCar );
-    m_treeCtrl.InsertItem( L"SD-397", hCar );
-    m_treeCtrl.InsertItem( L"JU-538", hCar );
-    m_treeCtrl.InsertItem( L"DI-285", hCar );
-    m_treeCtrl.InsertItem( L"AK-830", hCar );
-    hCar = m_treeCtrl.InsertItem( L"Compact", hItem );
-    m_treeCtrl.InsertItem( L"HG-490", hCar );
-    m_treeCtrl.InsertItem( L"PE-473", hCar );
-    hCar = m_treeCtrl.InsertItem( L"Standard", hItem );
-    m_treeCtrl.InsertItem( L"SO-398", hCar );
-    m_treeCtrl.InsertItem( L"DF-438", hCar );
-    m_treeCtrl.InsertItem( L"IS-833", hCar );
-    hCar = m_treeCtrl.InsertItem( L"Full Size", hItem );
-    m_treeCtrl.InsertItem( L"PD-304", hCar );
-    hCar = m_treeCtrl.InsertItem( L"Mini Van", hItem );
-    m_treeCtrl.InsertItem( L"ID-497", hCar );
-    m_treeCtrl.InsertItem( L"RU-304", hCar );
-    m_treeCtrl.InsertItem( L"DK-905", hCar );
-    hCar = m_treeCtrl.InsertItem( L"SUV", hItem );
-    m_treeCtrl.InsertItem( L"FE-948", hCar );
-    m_treeCtrl.InsertItem( L"AD-940", hCar );
-    hCar = m_treeCtrl.InsertItem( L"Truck", hItem );
-    m_treeCtrl.InsertItem( L"HD-394", hCar );
+    HTREEITEM hItem, hProcess;
+    hItem = m_treeCtrl.InsertItem( L"Processes", TVI_ROOT );
+
+    for( i = 0; i < cProcesses; i++ )
+    {
+        if( aProcesses[ i ] != 0 )
+        {
+            CString str;
+            str.Format( TEXT( "Process %d" ), aProcesses[ i ] );
+            m_treeCtrl.InsertItem( str, hItem );
+        }
+    }
 
     return TRUE;  // return TRUE  unless you set the focus to a control
 }
